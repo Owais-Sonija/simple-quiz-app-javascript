90f135160c31d4d8643a2b37824b9fb9f957b931
Owais-Sonija <owaissonija007@gmail.com>
2024-10-03 14:21:05 +0500

    Quiz completed with logic

--- docs/script.js
+++ docs/script.js
@@ -26,11 +26,15 @@ const quizQuestions = [
     }
 ];
 
-// Getting Elements
 let questionNumber = 0;
+let score = 0;
+// Getting Elements
 
 const quizContainerEle = document.querySelector(".quiz_container");
-
+// const optionsEle = document.querySelectorAll(".option")
+const nextBtn = document.querySelector(".next_btn");
+const scoreBtn = document.querySelector(".score_btn");
+const resetBtn = document.querySelector(".reset_btn");
 
 // Creating Functions
 
@@ -39,20 +43,85 @@ function renderQuestion(qNumber) {
         if (idx == qNumber) {
             quizContainerEle.innerHTML = `
             <div class="question my-6">
-                    <h2 class="question_text text-2xl font-semibold">${idx + 1}. ${question.questionText}</h2>
+                    <h2 class="question_text text-2xl font-semibold mb-4">${idx + 1}. ${question.questionText}</h2>
                     <div class="options">
-                        <ul>
+                        <div class="flex flex-col gap-5">
                             ${question.options.map((option) =>
-                `<li class="option py-2 px-3 border border-slate-800 rounded my-4 cursor-pointer hover:bg-slate-600 hover:border-slate-50 hover:text-slate-50 transition-all duration-150 ease-in-out">${option}</li>`
+                `<button data-answer="${option}" class="option disabled:cursor-not-allowed text-left py-2 px-3 border border-slate-800 rounded  cursor-pointer hover:bg-slate-600 hover:border-slate-50 hover:text-slate-50 transition-all duration-150 ease-in-out">${option}</button>`
             ).join("")}
-                        </ul>
+                        </div>
                     </div>
                 </div>
             `
         }
     })
+
+    const buttons = document.querySelectorAll(".option");
+    buttons.forEach((button) => {
+        button.addEventListener("click", (e) => {
+            selectOption(e);
+            buttons.forEach(btn => btn.disabled = true)
+
+
+        })
+    })
+
+}
+
+function nextQuestion() {
+    nextBtn.classList.add("hidden")
+    if (questionNumber < quizQuestions.length) {
+        questionNumber++;
+        renderQuestion(questionNumber)
+    }
+}
+
+function selectOption(e) {
+    if (questionNumber <= 3) {
+        nextBtn.classList.remove("hidden");
+
+    } else if (questionNumber == 4) {
+        scoreBtn.classList.remove("hidden")
+    }
+    quizQuestions.map((question, idx) => {
+        if (idx == questionNumber) {
+
+            if (question.correctAnswer === e.target.dataset.answer) {
+                e.target.classList.add("bg-green-500");
+                score++;
+            } else if (question.correctAnswer !== e.target.dataset.answer) {
+                console.log("qAnswer", question.correctAnswer);
+                console.log("data", e.target.dataset.answer);
+                document.querySelectorAll(".option").forEach(option => option.dataset.answer == question.correctAnswer ? option.classList.add("bg-green-500") : "")
+                e.target.classList.add("bg-red-500");
+            }
+        }
+    })
+}
+
+function displayScore() {
+    quizContainerEle.innerHTML = `
+    <div class="py-20 mb-5">
+        <h1 class="text-3xl font-bold text-slate-800">
+            Your score is ${score} / 5.
+        </h1>
+    </div>
+    `;
+    scoreBtn.classList.add("hidden");
+    resetBtn.classList.remove("hidden")
+}
+
+function resetQuiz() {
+    score = 0;
+    questionNumber = 0;
+    resetBtn.classList.add("hidden")
+    renderQuestion(questionNumber);
 }
 
 
+
 // Calling Functions
-renderQuestion(questionNumber)
+renderQuestion(questionNumber);
+nextBtn.addEventListener("click", nextQuestion)
+scoreBtn.addEventListener("click", displayScore)
+resetBtn.addEventListener("click", resetQuiz)
